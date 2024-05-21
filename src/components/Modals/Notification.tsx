d30b6692927cbd13ec94a81f097a929ac1ceeb16
MernFullStackBoy <jahongirmurtozayev777@gmail.com>
2024-05-21 11:17:57 +0500

    Udpate

--- src/components/Modals/Notification.tsx
+++ src/components/Modals/Notification.tsx
@@ -8,7 +8,7 @@ interface ModalProp {
 
 const Notification = ({ show, onClick, dark }: ModalProp) => {
     return (
-        <div className={` w-[100%] ${dark ? "text-white" : "text-slate-400"} backdrop-blur-[10px] left-0 h-[100vh] ${show ? "flex" : "hidden"} justify-center items-center absolute top-0 `}>
+        <div className={` w-[100%] ${dark ? "text-white" : "text-slate-400"} z-50 backdrop-blur-[10px] left-0 h-[100vh] ${show ? "flex" : "hidden"} justify-center items-center absolute top-0 `}>
             <div className={`w-[600px] flex justify-center items-center phone:w-[100%] phone:h-[250px] h-[300px] ${dark ? "bg-slate-800" : "bg-white"} shadow-2xl rounded-[20px]`}>
                 <button onClick={onClick} className=" fas fa-xmark text-[red] relative phone:bottom-[100px] bottom-[120px] text-[30px] left-[400px] phone:left-[250px] " />
                 <h1 className=" phone:text-[30px] font-semibold text-[40px] ">No Notification yet</h1>
--- src/components/Modals/VisitorsModal.tsx
+++ src/components/Modals/VisitorsModal.tsx
@@ -6,7 +6,7 @@ type VisitorsModalProps = {
 
 const VisitorsModal = ({onShow}: VisitorsModalProps) => {
     return (
-        <div className=" w-[100%] h-[100vh] backdrop-blur-[10px] absolute top-0 left-0 ">
+        <div className=" w-[100%] h-[100vh] backdrop-blur-[10px] z-50 absolute top-0 left-0 ">
             <button onClick={onShow} className=" bg-[red] fas fa-xmark text-[40px] rounded-[10px] px-[10px] py-[5px] text-white m-[30px] absolute right-0 " />
         </div>
     )
